89012a07851db66b286324a4f6b8e0d45fbe6244
Ravi Singh <ravi@appsimpact.com>
2021-04-15 16:12:23 +0530

    Added navigation bar

--- src/components/UI/Layout.js
+++ src/components/UI/Layout.js
@@ -1,9 +1,16 @@
+import React from 'react';
+
+import Navigation from './Navigation';
+
 const Layout = (props) => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-10 mx-md-auto">
-          {props.children}
+    <div>
+      <Navigation />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-10 mx-md-auto">
+            {props.children}
+          </div>
         </div>
       </div>
     </div>
